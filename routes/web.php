ba741a352b4a38ea1254f31f661ce4063f17d8fc
Farbod Heydari <kharazmifrequency@gmail.com>
2024-06-08 19:26:36 -0600

    simple-book-review-project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use \App\Http\Controllers\ReviewController;
+use GuzzleHttp\Client;
+Route::get('/', function () {
+    return redirect()->route("books.index");
+});
+Route::resource('books',BookController::class)->only(['index','show']);
+Route::resource('books.reviews',ReviewController::class)
+    ->scoped(['review','book'])
+    ->only(['create','store']);
